6d60f502e35a86903c4c953bdd015f027f132362
srivatsan <vatsank@gmail.com>
2021-08-02 10:27:03 +0530

    Update

---
+++ html/scripts/attachClickEvent.js
@@ -0,0 +1,22 @@
+
+function show(){
+    console.log('hello world');
+}
+
+var app={};
+
+
+
+app.attachEvents= function(){
+
+
+    var button = 
+          document.getElementById("btn1");
+
+      button.addEventListener('click',function(){
+
+           console.log("button clicked");
+      });    
+}
+
+app.attachEvents();
